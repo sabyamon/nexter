13094b2753f147313435d1e983518c1bc92bc762
Chris Millar <cmillar@adobe.com>
2024-04-09 10:43:14 -0600

    Fix multiple styles loading

--- nx/utils/styles.js
+++ nx/utils/styles.js
@@ -2,11 +2,16 @@ const STYLES = {};
 
 export default async function getStyle(href) {
   const path = href.replace('.js', '.css');
-  if (STYLES[path]) return STYLES[path];
-  const resp = await fetch(path);
-  const text = await resp.text();
-  const style = new CSSStyleSheet();
-  style.replace(text);
-  STYLES[path] = style;
-  return style;
+  if (!STYLES[path]) {
+    STYLES[path] = new Promise((resolve) => {
+      (async () => {
+        const resp = await fetch(path);
+        const text = await resp.text();
+        const style = new CSSStyleSheet();
+        style.replace(text);
+        resolve(style);
+      })();
+    });
+  }
+  return STYLES[path];
 }
