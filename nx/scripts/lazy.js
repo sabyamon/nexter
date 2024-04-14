60375657a3bf0488c17bd3af7130b95146e51a02
Chris Millar <cmillar@adobe.com>
2024-04-14 10:40:42 -0600

    Small branding fix

--- nx/scripts/lazy.js
+++ nx/scripts/lazy.js
@@ -2,6 +2,7 @@
   import('../deps/rum.js').then(({ sampleRUM }) => {
     sampleRUM('load');
     sampleRUM('lazy');
+    sampleRUM('cwv');
     sampleRUM.observe(document.querySelectorAll('main div[data-block-name]'));
     sampleRUM.observe(document.querySelectorAll('main picture > img'));
   });
