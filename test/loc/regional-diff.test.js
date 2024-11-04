aebb4901eb8cc4d32bd9696735ad469744f5db4b
ChrisChrisChris <chrischrischris@users.noreply.github.com>
2024-11-04 10:18:49 -0500

    Add test for regional-diff; Import convertUrl

---
+++ test/loc/regional-diff.test.js
@@ -0,0 +1,19 @@
+import { expect } from '@esm-bundle/chai';
+import { readFile } from '@web/test-runner-commands';
+import { regionalDiff, normalizeHTML } from '../../nx/blocks/locfour/regional-diff/regional-diff.js';
+
+function cleanHtmlWhitespace(html) {
+  return normalizeHTML(html).replace(/\s+/g, ' ').trim();
+}
+
+describe('Regional diff', () => {
+  it('Returns html with differences annotated', async () => {
+    const original = document.implementation.createHTMLDocument();
+    original.body.innerHTML = await readFile({ path: './mocks/lang-content.html' });
+    const modified = document.implementation.createHTMLDocument();
+    modified.body.innerHTML = await readFile({ path: './mocks/regional-content.html' });
+    const mainEl = await regionalDiff(original, modified);
+    const expectedDiffedMain = await readFile({ path: './mocks/diffedMain.html' });
+    expect(cleanHtmlWhitespace(mainEl.outerHTML)).to.equal(cleanHtmlWhitespace(expectedDiffedMain));
+  });
+});
