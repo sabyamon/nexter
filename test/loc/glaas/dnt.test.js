47426ce240418c2606c983ec42317ba5f98a2cb2
ChrisChrisChris <chrischrischris@users.noreply.github.com>
2025-02-05 15:48:26 -0500

    Add another testcase

--- test/loc/glaas/dnt.test.js
+++ test/loc/glaas/dnt.test.js
@@ -19,4 +19,12 @@ describe('Glaas DNT', () => {
     const expectedHtmlWithoutDnt = await readFile({ path: './mocks/dnt-removed.html' });
     expect(collapseWhitespace(htmlWithoutDnt)).to.equal(collapseWhitespace(expectedHtmlWithoutDnt));
   });
+
+  it('test Converts html to dnt formatted html', async () => {
+    const config = JSON.parse((await readFile({ path: './mocks/hubspot/translate.json' })));
+    const expectedHtmlWithDnt = await readFile({ path: './mocks/hubspot/post-dnt.html' });
+    const mockHtml = await readFile({ path: './mocks/hubspot/hubspot.html' });
+    const htmlWithDnt = await addDnt(mockHtml, config, { reset: true });
+    expect(collapseWhitespace(htmlWithDnt, true)).to.equal(collapseWhitespace(expectedHtmlWithDnt));
+  });
 });
